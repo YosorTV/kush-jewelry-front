3275963b6226efce29767d30c5f6701a475e1f8a
evhenii.alieksnadrov <yosorit@gmail.com>
2024-11-22 21:13:46 +0200

    fix: ui

--- components/simple/ProductCard/index.tsx
+++ components/simple/ProductCard/index.tsx
@@ -19,18 +19,20 @@ export const ProductCard: FC<ProductCardProps> = ({
 }) => {
   return (
     <figure className={cn('grid cursor-pointer overflow-hidden', className)}>
-      <AnimatedImage product={product} className={imgClassName} />
-      <span className='absolute left-0 top-0 z-[3] bg-neutral p-2 text-base-300'>{product.hintText}</span>
-      {product.saleValue > 0 && (
-        <span className='absolute left-0 top-12 z-[3] bg-neutral p-2 text-base-300'>-{product.saleValue}%</span>
-      )}
-      <Wishlist
-        className='absolute !right-0 top-0 !z-[3]'
-        session={session}
-        locale={product.locale}
-        productId={product.id}
-        inWishlist={product?.inWishlist ?? false}
-      />
+      <div className='relative'>
+        <AnimatedImage product={product} className={imgClassName} />
+        <span className='absolute left-0 top-0 z-[3] bg-neutral p-2 text-base-300'>{product.hintText}</span>
+        {product.saleValue > 0 && (
+          <span className='absolute left-0 top-12 z-[3] bg-neutral p-2 text-base-300'>-{product.saleValue}%</span>
+        )}
+        <Wishlist
+          className='absolute !right-0 top-0 !z-[3]'
+          session={session}
+          locale={product.locale}
+          productId={product.id}
+          inWishlist={product?.inWishlist ?? false}
+        />
+      </div>
       <figcaption className='flex w-full flex-col pt-2.5'>
         <div className='flex min-h-6 flex-1 items-baseline justify-between'>
           <NextLink href={`/catalog/${product.slug}`}>
