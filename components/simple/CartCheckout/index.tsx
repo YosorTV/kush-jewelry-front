a02b16a6260e6a017fbe7f00967a6d28cc257e1f
evhenii.alieksnadrov <yosorit@gmail.com>
2025-01-19 13:35:32 +0200

    fix: checkout

--- components/simple/CartCheckout/index.tsx
+++ components/simple/CartCheckout/index.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { useLocale, useTranslations } from 'next-intl';
-import { FC, useCallback, useEffect, useMemo, useRef } from 'react';
+import { useTranslations } from 'next-intl';
+import { FC, useEffect, useRef } from 'react';
 
 import { liqPayAdapter } from '@/adapters/payment';
 import { formatPrice } from '@/helpers/formatters';
@@ -18,7 +18,6 @@ interface ICartCheckout {
 }
 
 const CartCheckout: FC<ICartCheckout> = ({ currency, liqPayData }) => {
-  const locale = useLocale();
   const cartStore = useCart();
   const router = useRouter();
   const t = useTranslations('material');
@@ -27,49 +26,44 @@ const CartCheckout: FC<ICartCheckout> = ({ currency, liqPayData }) => {
   const liqPayContainerRef = useRef<HTMLDivElement | null>(null);
   const liqPayInstanceRef = useRef<any>(null);
 
-  const products = useMemo(() => {
-    return cartStore.cart.map((item: CartItemType) => ({
-      id: item.id,
-      name: item.name,
-      quantity: item.quantity,
-      images: item.images,
-      price: formatPrice(cartStore.totalPrice, currency).replace(/[^\d.,-]/g, ''),
-      url: item.url,
-      size: item?.size,
-      material: `${t(item?.material)}`
-    }));
-  }, [cartStore.cart, locale, currency]);
+  const products = cartStore.cart.map((item: CartItemType) => ({
+    id: item.id,
+    name: item.name,
+    quantity: item.quantity,
+    images: item.images,
+    price: formatPrice(cartStore.totalPrice, currency).replace(/[^\d.,-]/g, ''),
+    url: item.url,
+    size: item?.size,
+    material: `${t(item?.material)}`
+  }));
 
-  const debouncedCallback = useCallback(
-    debounce(async ({ data, signature, paytype, status }) => {
-      const payload = {
-        data,
-        signature,
-        status,
-        paytype,
-        products,
-        userId: Number(session?.user?.id) || null,
-        customer: {
-          firstName: cartStore.delivery.firstName,
-          lastName: cartStore.delivery.lastName,
-          email: cartStore.delivery.email,
-          phone: cartStore.delivery.phoneNumber,
-          customer_city: cartStore.delivery.self ? '' : cartStore.delivery.novapostCity.label,
-          customer_warehouse: cartStore.delivery.self ? '' : cartStore.delivery.novapostWarehouse.label,
-          self_delivery: cartStore.delivery.self
-        }
-      };
-
-      const result = await paymentCallback(payload);
-
-      if (result.status === 200) {
-        router.push(`/?checkout=success`);
-        cartStore.globalReset();
-        cartStore.setForm('success');
+  const debouncedCallback = debounce(async ({ data, signature, paytype, status }) => {
+    const payload = {
+      data,
+      signature,
+      status,
+      paytype,
+      products,
+      userId: Number(session?.user?.id) || null,
+      customer: {
+        firstName: cartStore.delivery.firstName,
+        lastName: cartStore.delivery.lastName,
+        email: cartStore.delivery.email,
+        phone: cartStore.delivery.phoneNumber,
+        customer_city: cartStore.delivery.self ? '' : cartStore.delivery.novapostCity.label,
+        customer_warehouse: cartStore.delivery.self ? '' : cartStore.delivery.novapostWarehouse.label,
+        self_delivery: cartStore.delivery.self
       }
-    }, 1000),
-    [cartStore.delivery, products, session]
-  );
+    };
+
+    const result = await paymentCallback(payload);
+
+    if (result.status === 200) {
+      cartStore.globalReset();
+      cartStore.setForm('success');
+      router.push(`/?checkout=success`);
+    }
+  }, 300);
 
   useEffect(() => {
     if (liqPayData?.data && liqPayData?.signature) {
@@ -94,9 +88,10 @@ const CartCheckout: FC<ICartCheckout> = ({ currency, liqPayData }) => {
       if (liqPayInstanceRef.current) {
         liqPayInstanceRef.current.destroy?.();
         liqPayInstanceRef.current = null;
+        cartStore.globalReset();
       }
     };
-  }, [liqPayData, debouncedCallback]);
+  }, [liqPayData]);
 
   return <div ref={liqPayContainerRef} className='h-full w-full overflow-hidden pb-10 pt-5' id='liqpay_checkout' />;
 };
