3fda97ae36109706a78c17114bf67df8253aefb0
evhenii.alieksnadrov <yalieksnadrov@solvexus.com>
2025-06-14 19:51:59 +0300

    refactor: replace AnimatedTag with semantic figure element in ProductCard component

--- components/simple/ProductCard/index.tsx
+++ components/simple/ProductCard/index.tsx
@@ -8,7 +8,6 @@ import { cn } from '@/lib';
 import { ProductCardProps } from '@/types/components';
 import { Price } from '../Price';
 import { Wishlist } from '../Wishlist';
-import { AnimatedTag } from '../AnimatedTag';
 
 export const ProductCard: FC<ProductCardProps> = ({
   product,
@@ -18,7 +17,7 @@ export const ProductCard: FC<ProductCardProps> = ({
   currency = 41
 }) => {
   return (
-    <AnimatedTag tag='figure' className={cn('grid cursor-pointer overflow-hidden', className)}>
+    <figure className={cn('grid cursor-pointer overflow-hidden', className)}>
       <div className='relative'>
         <AnimatedImage product={product} className={imgClassName} />
         <span className='absolute left-0 top-0 z-[3] bg-neutral p-2 text-base-300'>{product.hintText}</span>
@@ -43,6 +42,6 @@ export const ProductCard: FC<ProductCardProps> = ({
           <Price currency={currency} price={product?.price} sale={product?.saleValue} />
         </div>
       </figcaption>
-    </AnimatedTag>
+    </figure>
   );
 };
